77aa7075e53d7b2c3a69ebb9af740c6e8d6f5581
mcapri27 <juanlyonne06@gmail.com>
2024-11-15 15:57:31 +0200

    Update printifyImageFunctionality.js

--- printifyImageFunctionality.js
+++ printifyImageFunctionality.js
@@ -19,13 +19,13 @@
 	}
 	
 	function setImg(){
-		return slider_img.setAttribute("src", "file:///C:/docs/Professor%20Yard/" + images[i]);
+		return slider_img.setAttribute("src", "printifyImages/" + images[i]);
 	}
 	
 	var slider_img2	= document.querySelector(".mugProductPic2");
 	var images2 = ["Context.png",
-					"Front%20and%20Back.png", 
-					"Front%20and%20Side.png"];
+					"Front and Back.png", 
+					"Front and Side.png"];
 	var i = 0;
 	
 	function prev2(){
@@ -41,16 +41,16 @@
 	}
 	
 	function setImg2(){
-		return slider_img2.setAttribute("src", "file:///C:/docs/Professor%20Yard/" + images2[i]);
+		return slider_img2.setAttribute("src", "printifyImages/" + images2[i]);
 	}
 	
 	function setImg(){
-		return slider_img.setAttribute("src", "file:///C:/docs/Professor%20Yard/" + images[i]);
+		return slider_img.setAttribute("src", "printifyImages/" + images[i]);
 	}
 	
 	var slider_img3	= document.querySelector(".mugProductPic3");
-	var images3 = ["TumblerContext%202.png",
-					"TumblerContext%201.png",
+	var images3 = ["TumblerContext 2.png",
+					"TumblerContext 1.png",
 					"TumblerClose-up.png",
 					"Open.png", 
 					"TumblerRight.png",
@@ -72,13 +72,13 @@
 	}
 	
 	function setImg3(){
-		return slider_img3.setAttribute("src", "file:///C:/docs/Professor%20Yard/" + images3[i]);
+		return slider_img3.setAttribute("src", "printifyImages/" + images3[i]);
 	}
 	
 	//number 4
 	var slider_img4	= document.querySelector(".mugProductPic4");
-	var images4 = ["FernFloorMatContext%202.png",
-					"FernFloorMatContext%201.png",
+	var images4 = ["FernFloorMatContext 2.png",
+					"FernFloorMatContext 1.png",
 					"FernFloorMatFront.png",
 					"FernFloorMatDesign.png"];
 	var i = 0;
@@ -96,14 +96,14 @@
 	}
 	
 	function setImg4(){
-		return slider_img4.setAttribute("src", "file:///C:/docs/Professor%20Yard/" + images4[i]);
+		return slider_img4.setAttribute("src", "printifyImages/" + images4[i]);
 	}
 	
 	//number 5
 	var slider_img5	= document.querySelector(".mugProductPic5");
-	var images5 = ["MCcCandleContext%201.png",
-					"MCcCandleContext%203.png",
-					"MCcCandleContext%202.png",
+	var images5 = ["MCcCandleContext 1.png",
+					"MCcCandleContext 3.png",
+					"MCcCandleContext 2.png",
 					"MCcCandleFront.png", 
 					"MCcCandleOpen.png",
 					"MCccCandleDesign.png"];
@@ -122,16 +122,16 @@
 	}
 	
 	function setImg5(){
-		return slider_img5.setAttribute("src", "file:///C:/docs/Professor%20Yard/" + images5[i]);
+		return slider_img5.setAttribute("src", "printifyImages/" + images5[i]);
 	}
 	
 	//number 6
 	var slider_img6	= document.querySelector(".mugProductPic6");
 	var images6 = ["ClockClose-up.png",
 					"ClockFront.png",
-					"ClockContext%202.png",
-					"ClockContext%203.png",
-					"ClockContext%201.png"];
+					"ClockContext 2.png",
+					"ClockContext 3.png",
+					"ClockContext 1.png"];
 	var i = 0;
 	
 	function prev6(){
@@ -147,6 +147,6 @@
 	}
 	
 	function setImg6(){
-		return slider_img6.setAttribute("src", "file:///C:/docs/Professor%20Yard/" + images6[i]);
+		return slider_img6.setAttribute("src", "printifyImages" + images6[i]);
 	}
 	
